d4830a6926980feb6bdc8d1fc7a8f88b7a6a4e20
timmythetiny <account@timharding.co>
2018-06-12 21:53:25 -0700

    Initial commit
    Project reboot!

--- PieMenu/MainWindow.xaml.cs
+++ PieMenu/MainWindow.xaml.cs
@@ -9,20 +9,52 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Forms;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Gma.System.MouseKeyHook;
 
 namespace PieMenu
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        IKeyboardEvents GlobalHook;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Hide();
+            this.ShowActivated = false; // Just pop over, don't steal focus
+
+            GlobalHook = Hook.GlobalEvents();
+            GlobalHook.KeyDown += GlobalHook_KeyDown;
+            GlobalHook.KeyUp += GlobalHook_KeyUp;
+        }
+
+
+        private void GlobalHook_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                this.Show();
+                this.Topmost = true;
+
+                Matrix tx = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
+                System.Drawing.Point pos = System.Windows.Forms.Control.MousePosition;
+                Point mouse = tx.Transform(new Point(pos.X, pos.Y));
+                Left = mouse.X - ActualWidth / 2.0;
+                Top = mouse.Y - ActualHeight / 2.0;
+            }
+        }
+
+        private void GlobalHook_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                this.Hide();
+            }
         }
     }
 }
